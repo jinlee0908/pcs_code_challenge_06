bdc74559c4ddabaf8b689849192a02316823d627
JinLee <jinlee0908@gmail.com>
2014-03-04 15:23:05 -0800

    to correct what i messed up

--- long_con/long_con_app.rb
+++ long_con/long_con_app.rb
@@ -16,17 +16,23 @@ post '/thanks' do
   erb:thanks
 end
 
+get '/suckers' do
+  @people = @test_data
+  # build a fake array of hashes
+  # to use in the view
+
+  erb:suckers
+end
+
 get '/suckers/:id' do 
   # @people = [@test_data.select {|person| person[:id] == params[:id]}] 
   id = params[:id].to_i - 1
-  all_people = [
-    {id:1, name:'Mrs. Theresa E. Stamm', email:'kieran@runte.biz', phone:'1-678-523-6736', twitter:'@Reinger'},
-    {id:2, name:'Keara Maggio', email:'cayla@lubowitz.com',phone:'1-399-471-4388 x9581', twitter:'@Weber'}
-  ]
+  all_people = @test_data
   @people = [all_people[id]]
   # build a fake array of hashes
   # to use in the view,
   # but limit to just the row siginified by the :id 
+  erb:suckers_specific
 end
 
 # end
